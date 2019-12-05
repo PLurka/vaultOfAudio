231035e8a121d7badf9e0cd592a8f3f76bce4ef9
PLurka <pawellurka@gmail.com>
2019-12-06 00:41:11 +0100

    Working version playing music, no effects yet

--- src/main/webapp/app/player/player.component.ts
+++ src/main/webapp/app/player/player.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AudioService } from '../services/audio/audio.service';
+import { CloudService } from '../services/cloud.service';
+import { StreamState } from '../interfaces/stream-state';
 
 @Component({
   selector: 'jhi-player',
@@ -7,23 +10,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayerComponent implements OnInit {
   message: string;
-  files: Array<any>;
-  state;
-  currentFile: any;
+  files: Array<any> = [];
+  state: StreamState;
+  currentFile: any = {};
 
-  constructor() {
-    this.message = 'PlayerComponent message';
-    this.files = [{ name: 'First Song', artist: 'Inder' }, { name: 'Second Song', artist: 'You' }];
-    this.currentFile = {};
+  constructor(public audioService: AudioService, public cloudService: CloudService) {
+    // get media files
+    cloudService.getFiles().subscribe(files => {
+      this.files = files;
+    });
+
+    // listen to stream state
+    this.audioService.getState().subscribe(state => {
+      this.state = state;
+    });
+  }
+
+  playStream(url) {
+    this.audioService.playStream(url).subscribe(events => {
+      // listening for fun here
+    });
+  }
+
+  openFile(file, index) {
+    this.currentFile = { index, file };
+    this.audioService.stop();
+    this.playStream(file.url);
+  }
+
+  pause() {
+    this.audioService.pause();
+  }
+
+  play() {
+    this.audioService.play();
+  }
+
+  stop() {
+    this.audioService.stop();
+  }
+
+  next() {
+    const index = this.currentFile.index + 1;
+    const file = this.files[index];
+    this.openFile(file, index);
+  }
+
+  previous() {
+    const index = this.currentFile.index - 1;
+    const file = this.files[index];
+    this.openFile(file, index);
   }
 
   ngOnInit() {}
 
   isFirstPlaying() {
-    return false;
+    return this.currentFile.index === 0;
   }
 
   isLastPlaying() {
-    return true;
+    return this.currentFile.index === this.files.length - 1;
+  }
+
+  onSliderChangeEnd(change) {
+    this.audioService.seekTo(change.value);
   }
 }
---
+++ src/main/webapp/app/services/cloud.service.ts
@@ -0,0 +1,32 @@
+// src/app/services/cloud.service.ts
+import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CloudService {
+  files: any = [
+    // tslint:disable-next-line: max-line-length
+    {
+      url: '../../content/audio/8.mp3',
+      name: 'Good Time, Bad Times',
+      artist: 'The Ace of Void'
+    },
+    {
+      // tslint:disable-next-line: max-line-length
+      url: '../../content/audio/5.mp3',
+      name: 'Tough Love',
+      artist: 'The Ace of Void'
+    },
+    {
+      url: '../../content/audio/12.mp3',
+      name: 'Wander',
+      artist: 'The Ace of Void'
+    }
+  ];
+
+  getFiles() {
+    return of(this.files);
+  }
+}
