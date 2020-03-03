83fa9c26022bd38aa14194c66b1b6ee49f9e69a2
PLurka <pawellurka@gmail.com>
2020-03-03 01:10:20 +0100

    Corrected dependencies and refactored materials imports

--- src/main/webapp/app/material.module.ts
+++ src/main/webapp/app/material.module.ts
@@ -1,6 +1,11 @@
 // src/app/material.module.ts
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatListModule, MatSliderModule, MatIconModule, MatToolbarModule, MatCardModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
 
 const modules = [MatButtonModule, MatListModule, MatSliderModule, MatIconModule, MatToolbarModule, MatCardModule];
 
