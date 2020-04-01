1bdbf948d71b5051bdd09e6ff1560a934b884c60
PLurka <pawellurka@gmail.com>
2020-04-01 20:10:47 +0200

    Copied content from entities->song to my-songs

--- src/main/webapp/app/my-songs/my-songs.component.ts
+++ src/main/webapp/app/my-songs/my-songs.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { SongService } from 'app/entities/song';
+import { AccountService } from 'app/core';
+import { ISong } from 'app/shared/model/song.model';
+
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { Subscription } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+import { JhiEventManager, JhiAlertService } from 'ng-jhipster';
 
 @Component({
   selector: 'jhi-my-songs',
@@ -7,10 +15,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MySongsComponent implements OnInit {
   message: string;
+  songs: ISong[];
+  currentAccount: any;
+  eventSubscriber: Subscription;
 
-  constructor() {
+  constructor(
+    protected songService: SongService,
+    protected jhiAlertService: JhiAlertService,
+    protected eventManager: JhiEventManager,
+    protected accountService: AccountService
+  ) {
     this.message = 'MySongsComponent message';
   }
 
-  ngOnInit() {}
+  loadAll() {
+    this.songService
+      .query()
+      .pipe(
+        filter((res: HttpResponse<ISong[]>) => res.ok),
+        map((res: HttpResponse<ISong[]>) => res.body)
+      )
+      .subscribe(
+        (res: ISong[]) => {
+          this.songs = res;
+        },
+        (res: HttpErrorResponse) => this.onError(res.message)
+      );
+  }
+
+  ngOnInit() {
+    this.loadAll();
+    this.accountService.identity().then(account => {
+      this.currentAccount = account;
+    });
+    this.registerChangeInSongs();
+  }
+
+  ngOnDestroy() {
+    this.eventManager.destroy(this.eventSubscriber);
+  }
+
+  trackId(index: number, item: ISong) {
+    return item.id;
+  }
+
+  registerChangeInSongs() {
+    this.eventSubscriber = this.eventManager.subscribe('songListModification', response => this.loadAll());
+  }
+
+  protected onError(errorMessage: string) {
+    this.jhiAlertService.error(errorMessage, null, null);
+  }
 }
